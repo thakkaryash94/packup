9c885db028674bcc4a0cf9da2b4e3338859d9897
Yash Thakkar <thakkaryash94@gmail.com>
2018-09-14 02:10:09 +0530

    cache index page on server

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.prepare()
     const server = express()
 
     server.get('/', (req, res) => {
-      return app.render(req, res, '/', req.query)
+      renderAndCache(req, res, '/', req.query)
     })
 
     server.get('/api/repo', (req, res) => {
@@ -39,3 +39,42 @@ app.prepare()
       console.log(`> Ready on http://localhost:${port}`)
     })
   })
+
+
+/*
+ * NB: make sure to modify this to take into account anything that should trigger
+ * an immediate page change (e.g a locale stored in req.session)
+ */
+function getCacheKey (req) {
+  return `${req.url}`
+}
+
+async function renderAndCache (req, res, pagePath, queryParams) {
+  const key = getCacheKey(req)
+
+  // If we have a page in the cache, let's serve it
+  if (ssrCache.has(key)) {
+    res.setHeader('x-cache', 'HIT')
+    res.send(ssrCache.get(key))
+    return
+  }
+
+  try {
+    // If not let's render the page into HTML
+    const html = await app.renderToHTML(req, res, pagePath, queryParams)
+
+    // Something is wrong with the request, let's skip the cache
+    if (res.statusCode !== 200) {
+      res.send(html)
+      return
+    }
+
+    // Let's cache this page
+    ssrCache.set(key, html)
+
+    res.setHeader('x-cache', 'MISS')
+    res.send(html)
+  } catch (err) {
+    app.renderError(err, req, res, pagePath, queryParams)
+  }
+}
